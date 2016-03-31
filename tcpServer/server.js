07f0c5b805616d8f555d2fab9697ff16bb82230a
Valko Vova <valko.volodya@gmail.com>
2016-03-31 11:04:25 +0300

    Changed merge logic in 3rd task

--- tcpServer/server.js
+++ tcpServer/server.js
@@ -3,7 +3,7 @@ global.api = api;
 api.net = require('net');
 
 //array for computing
-var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 var result = [];
 
 var cashe = [];
@@ -11,21 +11,26 @@ var cashe = [];
 //pool of clients
 var clients = [];
 
+var numberOfClients = 3;
+
 var server = api.net.createServer(function(socket) {
   //print who`s connected
   console.log('Connected: ' + socket.localAddress);
 
   //just creating pull of clients
-  clients.push(socket);
+  //statuses : 0 - waiting
+  //           1 - computing
+  clients.push({status: 0, soc : socket});
   console.log("Client connect, count: ", clients.length);
 
   //fixed number of wanted clients
-  if (clients.length == 2){
+  if (clients.length == numberOfClients){
     for (var i = 0; i < clients.length; i++){
       //count number for dividing
       var H = array.length / clients.length;
       //divide and send to client
-      clients[i].write(JSON.stringify({id : i, array : array.slice(i * H, (i + 1) * H)}));
+      clients[i].soc.write(JSON.stringify({id : i, array : array.slice(i * H, (i + 1) * H)}));
+      clients[i].status = 1;
     }
   }
 
@@ -35,16 +40,12 @@ var server = api.net.createServer(function(socket) {
     //performing check, if recieved data is just what you need
     obj = JSON.parse(data);
     if (Array.isArray(obj.array)){
-      cashe.push(obj);
+      cashe[obj.id] = obj.array;
     }
-    //merge like a boss
+    //merge
     if (cashe.length == clients.length){
-      for (var i = 0; i < clients.length; i++){
-        for (var j = 0; j < clients.length; j++){
-          if (cashe[j].id == i){
-            result = result.concat(cashe[j].array);
-          }
-        }
+      for(var i = 0; i < cashe.length; i++){
+        result = result.concat(cashe[i]);
       }
     }
     //printing result only if we have it
